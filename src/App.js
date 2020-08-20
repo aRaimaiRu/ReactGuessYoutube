9abdef2e187fb93e787a515d1b83786a26901128
Pawut Jingjit <pawut.j@gmail.com>
2020-08-21 00:11:44 +0900

    add CSS color + font

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import YouTube from "react-youtube";
 import someobject from "./aaaaaa.json";
 //const data = require("./anydata.json");
 import { filterByVote, createNumeralArray } from "./util.js";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';  
 function App() {
   var obj = someobject;
   console.log(obj);
@@ -42,34 +44,37 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" style ={{'backgroundColor':'#eef5fa' , fontFamily: 'Comfortaa' }} >
+      
+      <div style = {{display: 'flex' , flexDirection : 'column' }}>
+        <h1 style = {{textAlign : 'center' , color:'	#719cad'}}>
+          ReactGuessYoutube
+        </h1>
+        <div style ={{margin:'auto',padding:20}}>
       <div style={{ display: isHide ? "block" : "none" }}>
         <YouTube videoId={url} opts={opts} onReady={onReady} />
       </div>
 
       <div style={{ display: !isHide ? "block" : "none" }}>
         <div
-          style={{ backgroundColor: "Black", height: 390, width: 640 }}
+          style={{ backgroundColor: '#496370', height: 390, width: 640 }}
         ></div>
       </div>
+      </div>
 
-      <button
-        onClick={() => {
-          console.log(obj.keys);
-        }}
-      >
-        log data
-      </button>
-      <button onClick={() => setIsHide(() => !isHide)}>toggle hide </button>
-      <button
+      <div style  ={{flexDirection:"column", textAlign:'center'}}>
+
+      <Button onClick={() => setIsHide(() => !isHide)} style ={{margin:20}}>toggle hide </Button>
+      <Button style ={{margin:20}}
         onClick={() => {
           seturl(randomURLV2());
           setIsHide(() => false);
         }}
       >
         random URL{" "}
-      </button>
-      <button onClick={() => filterEasy()}>Easy Mode </button>
+      </Button>
+      </div>
+    </div>
     </div>
   );
 }
